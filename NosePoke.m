79a1cca9407c03eef5da2cf411874e655000b3a7
antonioleeps <42138581+antonioleeps@users.noreply.github.com>
2022-10-03 19:03:38 +0200

    Added SessionDescription in Protocol and Setting
    In GUISetup, added an extra text box at Settings to indicate the goal of the task. In main script NosePoke, added the SessionDescription on the protocol and also features that push this info to SessionData together with Setting's SessionDescription

--- NosePoke.m
+++ NosePoke.m
@@ -84,8 +84,21 @@ while RunSession
         return
     end
     
+    % insert session description in protocol into data.info
+    if iTrial == 1
+        BpodSystem.Data.Info.SessionDescription = ["To teach the subject the nose poking sequence with correct timings"];
+        BpodSystem.Data.Custom.General.SessionDescription = BpodSystem.Data.Info.SessionDescription;
+    end
+
+    % append session description in setting into data.info
+    if TaskParameters.GUI.SessionDescription ~= BpodSystem.Data.Info.SessionDescription(end)
+        BpodSystem.Data.Info.SessionDescription = [BpodSystem.Data.Info.SessionDescription, TaskParameters.GUI.SessionDescription];
+        BpodSystem.Data.Custom.General.SessionDescription = BpodSystem.Data.Info.SessionDescription;
+    end
+    
     % update fields
-    UpdateCustomDataFields(iTrial)
+    UpdateCustomDataFields(iTrial);
+    SaveBpodSessionData();
     
     % update figures
     NosePoke_PlotSideOutcome(BpodSystem.GUIHandles.OutcomePlot,'update',iTrial);
