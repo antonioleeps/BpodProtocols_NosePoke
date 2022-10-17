02f4e22c6e1cef8475a30fff42f4affd44ae0ece
Greg Knoll <gregory@bccn-berlin.de>
2022-10-17 15:07:59 +0200

    Combine csv save scripts; rmv GUI param csv script...
    ...the parameters saved in save_GUI_params_csv.m
       are also stored for each trial in Data.TrialSettings.
       Therefore, save_GUI_params_csv.m was redundant and the
       parameter values for each trial could be appended to the
       custom data table in the custom data script.
       Some fields in the custom data were also redundant once
       the parameter values were saved, so these were removed.

--- save_custom_data_and_params_csv.m
+++ save_custom_data_and_params_csv.m
@@ -1,5 +1,4 @@
-function save_custom_data_csv()
-
+function save_custom_data_and_params_csv()
 %{
 Function to write trial custom data from NosePoke
 into a tab separated value file (.tsv)
@@ -15,6 +14,7 @@ n_trials = BpodSystem.Data.nTrials;
 trial_data = BpodSystem.Data.Custom.TrialData;
 
 %{
+---------------------------------------------------------------------------
 preprocess the data
 - remove last entry in arrays that are n_trials+1 long (from the incomplete
   last trial)
@@ -22,14 +22,13 @@ preprocess the data
 
 then save in the table as a column (requires using .', which inverts the
 dimensions)
+---------------------------------------------------------------------------
 %}
 data_table = table();
 
 % ---------------------Sample and Choice variables-------------------- %
 data_table.EarlyWithdrawal = trial_data.EarlyWithdrawal(1:n_trials).';
-data_table.SampleTime = trial_data.SampleTime(1:n_trials).';
 data_table.sample_length = trial_data.sample_length(1:n_trials).';
-
 data_table.ChoiceLeft = trial_data.ChoiceLeft(1:n_trials).';
 data_table.move_time = trial_data.move_time(1:n_trials).';
 data_table.port_entry_delay = trial_data.port_entry_delay(1:n_trials).';
@@ -43,16 +42,26 @@ data_table.RewardDelay = trial_data.RewardDelay(1:n_trials).';
 data_table.RewardMagnitude_L = trial_data.RewardMagnitude(1:n_trials, 1);
 data_table.RewardMagnitude_R = trial_data.RewardMagnitude(1:n_trials, 2);
 
-data_table.CenterPortRewAmount = trial_data.CenterPortRewAmount(1:n_trials).';
-data_table.CenterPortRewarded = trial_data.CenterPortRewarded(1:n_trials).';
 
 % -------------------------Misc variables------------------------------ %
-data_table.Jackpot = trial_data.Jackpot(1:n_trials).';
 data_table.RandomThresholdPassed = trial_data.RandomThresholdPassed(1:n_trials).';
 data_table.LightLeft = trial_data.LightLeft(1:n_trials).';
 
+
+% ----------------------------Params----------------------------------- %
+param_names = BpodSystem.GUIData.ParameterGUI.ParamNames;
+param_vals = BpodSystem.Data.TrialSettings.';
+params_table = cell2table(param_vals, "VariableNames", param_names);
+
+
+% --------------------------------------------------------------------- %
+% Combine the data and params tables and save to .csv
+% --------------------------------------------------------------------- %
+full_table = [data_table params_table];
+
 [filepath, session_name, ext] = fileparts(BpodSystem.Path.CurrentDataFile);
-file_name = string(strcat("O:\data\", session_name, "_trial_custom_data.csv"));
-writetable(data_table, file_name, "Delimiter", "\t")
+csv_name = "_trial_custom_data_and_params.csv";
+file_name = string(strcat("O:\data\", session_name, csv_name));
+writetable(full_table, file_name, "Delimiter", "\t")
 
 end  % save_custom_data_and_params_tsv()
