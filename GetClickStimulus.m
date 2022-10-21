90357f7ea73bab8692e2f15e324e927a756aaca8
antonioleeps <42138581+antonioleeps@users.noreply.github.com>
2022-10-21 12:44:35 +0200

    Update GetClickStimulus.m

--- GetClickStimulus.m
+++ GetClickStimulus.m
@@ -1,5 +1,11 @@
-function [LeftClickTrain,RightClickTrain] = GetClickStimulus(Duration, SamplingRate, ClickLength)
+function [LeftClickTrain,RightClickTrain] = GetClickStimulus(Duration, SamplingRate, ClickLength, Mode)
 
+global TaskParameters
+global BpodSystem
+
+if nargin<4
+    Mode = 'uniform';
+end
 if nargin<3
     ClickLength = 1;
 end
@@ -11,9 +17,16 @@ if nargin<1
 end
 
 % draw ClickRate for left and right sound
-rr = rand(1,1)*0.6+0.2;
-LeftClickRate = ceil(rr*100);
-RightClickRate = 100 - LeftClickRate;
+switch Mode
+    case 'uniform'
+        rr = rand(1,1)*0.6+0.2;
+        LeftClickRate = ceil(rr*100);
+        RightClickRate = 100 - LeftClickRate;
+    case 'beta'
+        BpodSystem.Data.Custom.AuditoryOmega(a) = betarnd(TaskParameters.GUI.AuditoryAlpha/4,TaskParameters.GUI.AuditoryAlpha/4,1,1);
+        LeftClickRate = round(BpodSystem.Data.Custom.AuditoryOmega(a)*TaskParameters.GUI.SumRates);
+        RightClickRate = TaskParameters.GUI.SumRates - LeftClickRate;
+end
 
 LeftClickTrain = GeneratePoissonClickTrain(LeftClickRate, Duration, SamplingRate, ClickLength);
 RightClickTrain = GeneratePoissonClickTrain(RightClickRate, Duration, SamplingRate, ClickLength);
